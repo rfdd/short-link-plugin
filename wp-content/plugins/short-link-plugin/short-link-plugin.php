518d2bc823b16b954922b576a0e297ad354f7455
rfd <gorjmy@gmail.com>
2023-11-24 20:05:36 +0300

    register  CPT 'Short Links'

--- wp-content/plugins/short-link-plugin/short-link-plugin.php
+++ wp-content/plugins/short-link-plugin/short-link-plugin.php
@@ -5,3 +5,13 @@ Description: Плагин для создания и управления кор
 Version: 1.0
 Author: rfd
 */
+
+function short_link_plugin_init() {
+    $args = [
+        'public' => true,
+        'label'  => 'Short Links',
+        // other arguments
+    ];
+    register_post_type('short_link', $args);
+}
+add_action('init', 'short_link_plugin_init');
